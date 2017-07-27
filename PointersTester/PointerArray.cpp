d6888c0b56aa8cfa13a6fbcd95c8161483701bf8
karel12 <wulf1212@gmail.com>
2017-07-27 11:58:44 +0100

    first

--- PointersTester/PointerArray.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  PointerArray.cpp
-//  PointersTester
-//
-//  Created by Karel Kuzmiak on 27/07/2017.
-//  Copyright © 2017 Karel Kuzmiak. All rights reserved.
-//
-
-#include "PointerArray.hpp"
--- PointersTester/PointersInFunctions.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  PointersInFunctions.cpp
-//  PointersTester
-//
-//  Created by Karel Kuzmiak on 27/07/2017.
-//  Copyright © 2017 Karel Kuzmiak. All rights reserved.
-//
-
-#include <stdio.h>
